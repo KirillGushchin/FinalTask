9ecb9540e7de057a3cea8268ca66bd03ae58c942
KirillGushchin <77499890+KirillGushchin@users.noreply.github.com>
2021-02-01 12:24:06 +0300

    Delete src/test directory

--- src/test/java/Utils/TextGenerator.java
+++
@@ -1,9 +0,0 @@
-package Utils;
-
-import org.apache.commons.lang3.RandomStringUtils;
-
-public class TextGenerator {
-    public static String generateText(int length){
-        return RandomStringUtils.randomAlphabetic(length);
-    }
-}
